de911f4bf254b51ab5e1ae36c81ef40f4cbccbbe
Molly Carlson <mollyc@email.sc.edu>
2018-10-10 15:47:17 -0400

    Added base CSS file
    Stripped out unecessary code in index.php
    
    added teeny bootstrap grid

--- SNIPPETS/HEADER.php
+++ SNIPPETS/HEADER.php
@@ -7,7 +7,7 @@
     <title> Pendleton Country Public Library </title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"/>
-    <link rel = "stylesheet" href = "CSS/index.css" />
+    <link rel = "stylesheet" href = "/Users/workaccount/LIS467Site/CSS/index.css" />
  </head>
 
   <body>
--- index.php
+++ index.php
@@ -1,32 +1,15 @@
 <?php require_once("/Users/workaccount/LIS467Site/SNIPPETS/HEADER.php"); ?>
 
-   <!-- <div class = "jumbotron jumbotron-fluid" id="HomeSection">  -->
-       <!-- <div class="container"> -->
-          <!--  <h1 class = "display-3 jumbotronTextOnImages text-center" id = "JumbotronHeading"> Heading </h1> -->
-            <!--<img id = "JumbotronImage" src = "https://res.cloudinary.com/sonarsystems/image/upload/v1497973720/Youtube_2Bnew_2Bheader_u0ruil.png" /> -->
-            
-            <!--<p class = "lead"> This is a modified jumbotron that occupies the entire horizontal space of its parent.</p> -->
-       <!-- </div> -->
-   <!-- </div> -->
-
-  <!--  <div class = "container">
-        <div class = "row">
-            <h1> Hello World! </h1>
+ <div class = "container-fluid">
+    <div class = "row">
+        <div class = "col-12 flex-column">
         </div>
-    </div> -->
-    <p>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-
-
-    </p>
+        <div class = "col-md-6">
+        </div>
+        <div class = "col-md-6">
+        </div>
+    </div>
+ </div>
 
 <?php require_once("/Users/workaccount/LIS467Site/SNIPPETS/FOOTER.php"); ?>
           
